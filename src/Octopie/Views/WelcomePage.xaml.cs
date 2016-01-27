8471b06903452a5be2a30ff47a238f1b85023be5
James Ko <jamesqko@gmail.com>
2016-01-27 17:17:24 -0500

    Rename MainPage to WelcomePage

--- src/Octopie/Views/WelcomePage.xaml.cs
+++ src/Octopie/Views/WelcomePage.xaml.cs
@@ -20,9 +20,9 @@ namespace Octopie.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class WelcomePage : Page
     {
-        public MainPage()
+        public WelcomePage()
         {
             this.InitializeComponent();
         }
