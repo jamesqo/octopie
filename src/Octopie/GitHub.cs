d662588a88456b67c78af369a8669f4f5f62643b
James Ko <jamesqko@gmail.com>
2016-01-27 15:20:02 -0500

    Add OAuth2 flow and a new page

---
+++ src/Octopie/GitHub.cs
@@ -0,0 +1,23 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopie
+{
+    public static class GitHub
+    {
+        public static string AccessToken { get; set; }
+
+        public static GitHubClient CreateClient(string token)
+        {
+            var headerValue = new ProductHeaderValue("Octopie");
+            return new GitHubClient(headerValue)
+            {
+                Credentials = new Octokit.Credentials(token)
+            };
+        }
+    }
+}
--- src/Octopie/LoginPage.xaml.cs
+++ src/Octopie/LoginPage.xaml.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,19 +27,12 @@ namespace Octopie
     public sealed partial class LoginPage : Page
     {
         private const string AuthUri = "https://github.com/login/oauth/authorize";
+        private const string CallbackUri = "https://github.com/jamesqo/octopie";
+        private const string TokenUri = "https://github.com/login/oauth/access_token";
 
         public LoginPage()
         {
             this.InitializeComponent();
-        }
-
-        private GitHubClient CreateClient(string token)
-        {
-            var headerValue = new ProductHeaderValue("Octopie");
-            return new GitHubClient(headerValue)
-            {
-                Credentials = new Octokit.Credentials(token)
-            };
         }
 
         private void OnBrowserLoaded(object sender, RoutedEventArgs e)
@@ -46,9 +41,42 @@ namespace Octopie
             Browser.Source = new Uri(uri);
         }
         
-        private void OnBrowserLoadCompleted(object sender, NavigationEventArgs e)
+        private async void OnBrowserLoadCompleted(object sender, NavigationEventArgs e)
         {
-            Debug.WriteLine(Browser.Source);
+            var uri = Browser.Source;
+            Debug.WriteLine(uri);
+
+            string code;
+            if (!TryGetCode(uri.Query, out code))
+                return;
+
+            Browser.Visibility = Visibility.Collapsed;
+            
+            string token = await GetTokenFromCode(code);
+            GitHub.AccessToken = token;
+
+            Frame.Navigate(typeof(HomePage));
+        }
+
+        private async Task<string> GetTokenFromCode(string code)
+        {
+            var uri = $"{TokenUri}?client_id={Credentials.AppId}&client_secret={Credentials.AppSecret}&code={code}";
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(uri, null);
+                var query = await response.Content.ReadAsStringAsync();
+
+                var decoder = new WwwFormUrlDecoder(query);
+                return decoder.GetFirstValueByName("access_token");
+            }
+        }
+
+        private bool TryGetCode(string query, out string code)
+        {
+            var decoder = new WwwFormUrlDecoder(query);
+            var entry = decoder.FirstOrDefault(e => e.Name == "code");
+            code = entry?.Value;
+            return entry != null;
         }
     }
 }
